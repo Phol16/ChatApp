ab17a3ebb5f7c124761a37926d1cb24604a58da4
Phol <Pholibertlim@gmail.com>
2023-03-16 20:21:15 +0800

    updated something

--- server/app.js
+++ server/app.js
@@ -2,7 +2,6 @@ import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
 import helmet from 'helmet'
-import { CloudinaryService } from './utils/cloudinaryService.js'
 
 const app = express()
 
@@ -12,11 +11,6 @@ app.use(cors());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({policy: 'cross-origin'}));
 
-app.set(CloudinaryService.cloudinaryName, new CloudinaryService(
-  process.env.CLOUDNAME,
-  process.env.APIKEY,
-  process.env.APISECRET
-))
 
 
 export default app
