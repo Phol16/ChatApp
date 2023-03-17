9c3ca548bcdbaee3615b5c6eec343fa029d317de
Phol <Pholibertlim@gmail.com>
2023-03-18 00:49:56 +0800

    feat: login function

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -2,15 +2,28 @@ import React from 'react'
 import style from '../style/logIn.module.css'
 
 const LoginPage = () => {
+
+  const handleSubmit = async(e)=>{
+    e.preventDefault()
+    const username = e.target[0].value
+
+      const response = await fetch('http://localhost:3500/users/logIn',{
+        method:'POST',
+        headers:{
+          'Content-Type': 'application/json'
+        },
+        body:JSON.stringify({username})
+      }).then((res)=>res.json())
+      console.log(response)
+  }
+
   return (
     <div>
       <h1 className={style.logInHeader}>ChatApp</h1>
-      <form className={style.logInForm}>
+      <form onSubmit={handleSubmit} className={style.logInForm}>
       <label htmlFor="username">Username:</label>
       <input type="username" id='username' name='username' placeholder='Username'/>
-      <label htmlFor="RoomId">Room ID:</label>
-      <input type="text" id='roomId' name='roomId' placeholder='Room Id'/>
-      <button>Log In</button>
+      <button type='submit'>Log In</button>
       </form>
     </div>
   )
