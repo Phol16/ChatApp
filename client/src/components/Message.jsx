13c8f473b7419eab26109055fd8d58f988beb867
Phol <Pholibertlim@gmail.com>
2023-03-20 20:33:47 +0800

    chat box Ui fixed

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -9,7 +9,7 @@ const Message = ({data, own}) => {
   return (
     <div className={own ? style.own : style.message}>
       <div className={style.messageTop}>
-      <img src="https://images.unsplash.com/photo-1678962304218-d4aea8fa7d8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" className={style.photo} />
+      <img src="https://images.unsplash.com/photo-1678962304218-d4aea8fa7d8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="Photo" className={`${style.photo} ${own ? style.hidden : null}`} />
         <p>{data.text}</p>
       </div>
       <div className={style.messageBottom}>{data.createdAt && <ReactTimeAgo date={new Date(data.createdAt)} locale="en-US"/> }</div>
