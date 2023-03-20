e574f6076c22927f77e1a41e44806d18a1c01f44
Phol <Pholibertlim@gmail.com>
2023-03-20 23:36:55 +0800

    fixed footer

--- client/src/layouts/LoginLayout.jsx
+++ client/src/layouts/LoginLayout.jsx
@@ -19,7 +19,7 @@ const LoginLayout = () => {
       <img src='https://scontent.fmnl25-2.fna.fbcdn.net/v/t39.8562-6/120973513_338186077283942_8148888802958728934_n.png?_nc_cat=102&ccb=1-7&_nc_sid=6825c5&_nc_ohc=u8ETh_dkhsoAX-vbPzp&_nc_ht=scontent.fmnl25-2.fna&oh=00_AfDC3f7v0XOn8CLfTY5mYXUxF6x5IJACLEsaiUPTi3KHxg&oe=641C8767' alt='Photo' className={style.logInImg} />
       <footer className={style.footer}>
         <p>
-          © Messenger clone. <span> The Apple and Google Play logos are trademarks of their respective owners. </span>
+          © Mini Messenger. <span> The logos are trademarks of their respective owners. </span>
         </p>
       </footer>
       <section className={style.fixedInfo} onClick={()=>{setOpen(!open)}}><span style={{display:`${open ? 'inline-block' : 'none'}`}}>To test the realtime feature in mini messenger. use different browser/application to log in as different user<br/><br/>initial request from server will take time, please wait. <br/> thank you!<br/><br/></span>{arrow}</section>
