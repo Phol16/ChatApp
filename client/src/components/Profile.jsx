094080a4cb3cb41f5d7c24ae8bbc68aed44869eb
Phol <Pholibertlim@gmail.com>
2023-03-20 20:18:28 +0800

    profile tab fixed UI

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -17,7 +17,7 @@ const Profile = ({profile,socket}) => {
         <div className={style.profileBox}>
           <img src='https://images.unsplash.com/photo-1678962304218-d4aea8fa7d8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80' alt='' className={style.photo} />
           <h2>{profile.fullName}</h2>
-          <h2>{profile.username}</h2>
+          <h3>@{profile.username}</h3>
           <button className={style.logoutButton} onClick={handleLogout}>Logout</button>
         </div>
       ) : (
