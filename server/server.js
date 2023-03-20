0279ae02782f014091aa734086f497b227a6f39e
Phol <Pholibertlim@gmail.com>
2023-03-20 16:25:53 +0800

    disconnect socket

--- server/server.js
+++ server/server.js
@@ -27,18 +27,19 @@ const io = new Server(server, {
 
 let usersSocket = [];
 
+try {
+  
+
 const addUser = (userId, socketId) => {
-  !usersSocket.some((user) => user.usersId === userId) && usersSocket.push({ userId, socketId });
+  !usersSocket.some((user) => user.userId === userId) && usersSocket.push({ userId, socketId });
 };
 const removeUser = (socketId) => {
-  usersSocket = usersSocket.filter((user) =>  user.socketId !== socketId);
-  const userDisconnect = usersSocket.find(socket => socket.socketId === socketId)
-  usersSocket = usersSocket.filter((user) =>  user.userId !== userDisconnect?.userId);
+  usersSocket = usersSocket.filter((user) => user.socketId !== socketId);
 };
 
-const getUser = ((userId)=>{
-  return usersSocket.find(user=> user.userId === userId)
-})
+const getUser = (userId) => {
+  return usersSocket.find((user) => user.userId === userId);
+};
 
 io.on('connection', (socket) => {
   //when connect
@@ -49,14 +50,14 @@ io.on('connection', (socket) => {
   });
 
   //when sendmessage
-  socket.on('sendMessage',({senderId, receiverId, text})=>{
+  socket.on('sendMessage', ({ senderId, receiverId, text }) => {
     const user = getUser(receiverId);
-    io.to(user.socketId).emit('getMessage',{
+    io.to(user.socketId).emit('getMessage', {
       senderId,
       receiverId,
       text,
-    })
-  })
+    });
+  });
 
   //when disconnect
   socket.on('disconnect', () => {
@@ -64,11 +65,22 @@ io.on('connection', (socket) => {
     removeUser(socket.id);
     io.emit('getUsers', usersSocket);
   });
+  socket.on('disconnectMe', (userId) => {
+    const thisUser = usersSocket.find((user) => user.userId === userId);
+    if (thisUser) {
+      console.log('a user has disconnect');
+      removeUser(thisUser.socketId);
+      io.emit('getUsers', usersSocket);
+    }
+  });
 });
 
 server.listen(ChatPort, () => {
   console.log(`Chat App: ${ChatPort}`);
 });
+} catch (error) {
+  console.log(error)
+}
 
 mongoose.connect(mongoDB).then(() => {
   app.listen(port, () => {
