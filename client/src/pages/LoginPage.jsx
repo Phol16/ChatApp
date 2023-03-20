c94b600472067124b769d34da81cfac4875296b9
Phol <Pholibertlim@gmail.com>
2023-03-20 22:19:55 +0800

    added UX in login page

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -5,11 +5,13 @@ import {useNavigate} from 'react-router-dom'
 const LoginPage = () => {
   const[keepmelogin, setKeepmelogin] = useState(false)
   const[error, setError] = useState('')
+  const[load, setLoad] = useState(false)
   const navigate = useNavigate()
 
   const handleSubmit = async(e)=>{
     e.preventDefault()
     const username = e.target[0].value
+    setLoad(true)
     try {
       if(username){
       const response = await fetch('https://minimessengerserver.onrender.com/users/logIn',{
@@ -21,6 +23,7 @@ const LoginPage = () => {
       }).then((res)=>res.json())
       localStorage.setItem('User', response.data._id)
       navigate('/home')
+      setLoad(false)
     }else{setError('Input a username')}
     } catch (error) {
       setError(error)
@@ -33,8 +36,11 @@ const LoginPage = () => {
       <h3>Messenger makes it easy and fun to stay close to your favorite people.</h3>
       <form onSubmit={handleSubmit} className={style.logInForm}>
         {error && <p className={style.error}>{error}</p>}
-      <input type="username" id='username' name='username' placeholder='Username' onChange={()=>{setError('')}}/>
-      <button type='submit'>Enter</button>
+      <input type="username" id='username' name='username' placeholder='Username' onChange={()=>{setError(''),setLoad(false)}}/>
+      <button type='submit'>
+      <i className="fa fa-spinner fa-spin" style={{marginRight: '10px', display:`${load ? 'inline-block' : 'none'}`}}></i>
+        Enter
+        </button>
       <label htmlFor="keepmelogin" className={style.label} onClick={()=>{setKeepmelogin(!keepmelogin)}}>
       <input type="radio" name='keepmelogin' username='keepmelogin' id='keepmelogin' checked={keepmelogin} onChange={()=>{setKeepmelogin(!keepmelogin)}}/> 
         <p>Keep me login</p>
